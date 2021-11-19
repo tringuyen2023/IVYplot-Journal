beb155733d38937473d992b34b47fded680042a7
tringuyen2023 <78704304+tringuyen2023@users.noreply.github.com>
2021-11-18 20:23:19 -0500

    Add files via upload

---
+++ IVYplot.R
@@ -0,0 +1,6 @@
+## ---- echo=FALSE, fig.cap='An IVY plot without printing the frequencies.', out.width='5in', out.height = '4in', fig.align='center', fig.pos='htbp'----
+knitr::include_graphics('Without_frequency.png')
+
+## ---- echo=FALSE, fig.cap='An IVY plot with frequencies printed.', out.width='5in', out.height = '4in', fig.align='center', fig.pos='htbp'----
+knitr::include_graphics('With_frequency.png')
+
